1f2c86d6606b5618f8abc21d0f0477fe444b3e40
arendma <arend1@uni-potsdam.de>
2022-10-18 13:10:42 +0200

    Add R setup file

---
+++ setup.r
@@ -0,0 +1,16 @@
+#setup script for phsophoprot
+packs = c("ggplot2", "xlsx", "gridExtra", "taxize", "ggfortify", "VennDiagram")
+bcpacks = c()
+
+for (pack in packs) {
+  if(!(requireNamespace(pack, quietly=TRUE))) {
+    install.packages(pack)
+  }
+}
+
+for (bcpack in bcpacks) {
+  if(!(requireNamespace(bcpack, quietly=TRUE))) {
+    library(BiocManager)
+    BiocManager::install(bcpack)
+  }
+}
